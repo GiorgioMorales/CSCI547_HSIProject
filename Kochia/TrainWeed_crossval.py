262b4cb01848378559a42c00b66acad84c1c468a
GiorgioMorales <giorgiomoralesluna@gmail.com>
2020-02-18 00:26:27 -0700

    Add metrics Pruned vs Not Pruned

--- Kochia/TrainWeed_crossval.py
+++ Kochia/TrainWeed_crossval.py
@@ -1,11 +1,9 @@
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 from operator import truediv
 import h5py
-from sklearn.decomposition import PCA
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score, cohen_kappa_score, confusion_matrix
 
 from networks import *
@@ -23,17 +21,13 @@ LOAD HDF5 FILE
 
 hdf5_file = h5py.File('weed_dataset_w25.hdf5', "r")
 train_x = np.array(hdf5_file["train_img"][...])
-# train_x = train_x / np.max(train_x)
-# train_x = np.clip(train_x, 0, 1)
-#train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], train_x.shape[2], train_x.shape[3], 1))
 train_y = np.array(hdf5_file["train_labels"][...])
 
 # Average consecutive bands
-img2 = np.zeros((train_x.shape[0], train_x.shape[1], train_x.shape[2], int(train_x.shape[3]/2)))
+img2 = np.zeros((train_x.shape[0], train_x.shape[1], train_x.shape[2], int(train_x.shape[3] / 2)))
 for n in range(0, train_x.shape[0]):
-    # Average consecutive bands
     for i in range(0, train_x.shape[3], 2):
-        img2[n, :, :, int(i/2)] = (train_x[n, :, :, i] + train_x[n, :, :, i + 1]) / 2.
+        img2[n, :, :, int(i / 2)] = (train_x[n, :, :, i] + train_x[n, :, :, i + 1]) / 2.
 
 train_x = img2
 
@@ -64,16 +58,16 @@ def categorical_accuracy(y_true, y_pred):
     return k.cast(k.equal(k.argmax(y_true, axis=-1),
                           k.argmax(y_pred, axis=-1)),
                   k.floatx())
-                  
-                                                                        
+
+
 def AA_andEachClassAccuracy(confusion_m):
     list_diag = np.diag(confusion_m)
     list_raw_sum = np.sum(confusion_m, axis=1)
     each_ac = np.nan_to_num(truediv(list_diag, list_raw_sum))
     average_acc = np.mean(each_ac)
     return each_ac, average_acc
-    
-                
+
+
 data = 'WEED'
 # Load model
 print("Loading model...")
@@ -90,8 +84,8 @@ for train, test in kfold.split(train_x, train_y):
     # Compile model
     model = hyper3dnet(img_shape=(windowSize, windowSize, train_x.shape[3], 1), classes=classes)
     model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['acc', categorical_accuracy])
-    #optimizer = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    #model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc', categorical_accuracy])
+    # optimizer = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    # model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc', categorical_accuracy])
     filepath = "weights-hyper3dnet" + data + str(ntrain) + "-best_3layers_4filters.h5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
@@ -102,20 +96,20 @@ for train, test in kfold.split(train_x, train_y):
     print(data + ": Training" + str(ntrain) + "begins...")
     history = model.fit(x=train_x[train], y=ytrain, validation_data=(train_x[test], ytest),
                         batch_size=32, epochs=ep, callbacks=callbacks_list)
-                        
+
     # Evaluate network
     k.set_learning_phase(0)
     model.load_weights("weights-hyper3dnet" + data + str(ntrain) + "-best_3layers_4filters.h5")
     ypred = model.predict(train_x[test])
-    
+
     # Calculate metrics
     oa = accuracy_score(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
     confusion = confusion_matrix(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
     each_acc, aa = AA_andEachClassAccuracy(confusion)
     kappa = cohen_kappa_score(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
     prec, rec, f1, support = precision_recall_fscore_support(np.argmax(ytest, axis=1),
-                                                            np.argmax(ypred, axis=-1), average='macro')
-    
+                                                             np.argmax(ypred, axis=-1), average='macro')
+
     # Add metrics to the list
     cvoa.append(oa * 100)
     cvaa.append(aa * 100)
@@ -123,11 +117,11 @@ for train, test in kfold.split(train_x, train_y):
     cvpre.append(prec * 100)
     cvrec.append(rec * 100)
     cvf1.append(f1 * 100)
-    
+
     print("%s: %.3f%%" % (model.metrics_names[1], oa * 100))
     file_name = "report_ntrain_" + str(ntrain) + ".txt"
     with open(file_name, 'w') as x_file:
-       x_file.write("Overall accuracy%.3f%%" % (float(oa)))
+        x_file.write("Overall accuracy%.3f%%" % (float(oa)))
 
     ntrain += 1
 
@@ -137,14 +131,14 @@ model.save(data + "_hyper3dnet_4layers_8filters.h5")
 
 file_name = "classification_report_" + data + ".txt"
 with open(file_name, 'w') as x_file:
-   x_file.write("Overall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvoa)), float(np.std(cvoa))))
-   x_file.write('\n')
-   x_file.write("Average accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvaa)), float(np.std(cvaa))))
-   x_file.write('\n')
-   x_file.write("Kappa accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvka)), float(np.std(cvka))))
-   x_file.write('\n')
-   x_file.write("Precision accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvpre)), float(np.std(cvpre))))
-   x_file.write('\n')
-   x_file.write("Recall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvrec)), float(np.std(cvrec))))
-   x_file.write('\n')
-   x_file.write("F1 accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvf1)), float(np.std(cvf1))))
+    x_file.write("Overall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvoa)), float(np.std(cvoa))))
+    x_file.write('\n')
+    x_file.write("Average accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvaa)), float(np.std(cvaa))))
+    x_file.write('\n')
+    x_file.write("Kappa accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvka)), float(np.std(cvka))))
+    x_file.write('\n')
+    x_file.write("Precision accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvpre)), float(np.std(cvpre))))
+    x_file.write('\n')
+    x_file.write("Recall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvrec)), float(np.std(cvrec))))
+    x_file.write('\n')
+    x_file.write("F1 accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvf1)), float(np.std(cvf1))))
---
+++ Kochia/Weed_metrics.py
@@ -0,0 +1,186 @@
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+from sklearn.model_selection import StratifiedKFold
+import numpy as np
+import pandas as pd
+from operator import truediv
+import h5py
+import pickle
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score, cohen_kappa_score, confusion_matrix
+
+from networks import *
+
+import keras.backend as k
+import tensorflow as tf
+
+k.set_image_data_format('channels_last')
+k.set_learning_phase(0)
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+LOAD HDF5 FILE
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+hdf5_file = h5py.File('weed_dataset_w25.hdf5', "r")
+train_x = np.array(hdf5_file["train_img"][...])
+train_y = np.array(hdf5_file["train_labels"][...])
+
+# Average consecutive bands
+img2 = np.zeros((train_x.shape[0], train_x.shape[1], train_x.shape[2], int(train_x.shape[3] / 2)))
+for n in range(0, train_x.shape[0]):
+    for i in range(0, train_x.shape[3], 2):
+        img2[n, :, :, int(i / 2)] = (train_x[n, :, :, i] + train_x[n, :, :, i + 1]) / 2.
+
+train_x = img2
+
+print(train_x.shape)
+train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], train_x.shape[2], train_x.shape[3], 1))
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+TRAIN PROPOSED NETWORK
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+windowSize = train_x.shape[1]
+classes = 3
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+cvoa = []
+cvaa = []
+cvka = []
+cvpre = []
+cvrec = []
+cvf1 = []
+
+
+def AA_andEachClassAccuracy(confusion_m):
+    list_diag = np.diag(confusion_m)
+    list_raw_sum = np.sum(confusion_m, axis=1)
+    each_ac = np.nan_to_num(truediv(list_diag, list_raw_sum))
+    average_acc = np.mean(each_ac)
+    return each_ac, average_acc
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+PREDICT AND CALCULATE METRICS
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+dataset = 'WEED'
+# Initialize
+confmatrices = np.zeros((10, int(classes), int(classes)))
+
+ntrain = 1
+for train, test in kfold.split(train_x, train_y):
+    ytest = to_categorical(train_y[test]).astype(np.int32)
+
+    model = hyper3dnet(img_shape=(windowSize, windowSize, train_x.shape[3], 1), classes=int(classes))
+    model.load_weights("cv_weights//weights-hyper3dnet" + dataset + str(ntrain) + "-best_3layers_4filters.h5")
+    ypred = model.predict(train_x[test])
+
+    sess = tf.Session()
+    with sess.as_default():
+        con_mat = tf.math.confusion_matrix(labels=np.argmax(ytest, axis=-1),
+                                           predictions=np.argmax(ypred, axis=-1)).eval()
+
+    con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=3)
+    classes_list = list(range(0, int(classes)))
+    con_mat_df = pd.DataFrame(con_mat_norm, index=classes_list, columns=classes_list)
+
+    confmatrices[ntrain - 1, :, :] = con_mat_df.values
+
+    # Calculate metrics
+    oa = accuracy_score(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
+    confusion = confusion_matrix(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
+    each_acc, aa = AA_andEachClassAccuracy(confusion)
+    kappa = cohen_kappa_score(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1))
+    prec, rec, f1, support = precision_recall_fscore_support(np.argmax(ytest, axis=1), np.argmax(ypred, axis=-1),
+                                                             average='macro')
+
+    # Add metrics to the list
+    cvoa.append(oa * 100)
+    cvaa.append(aa * 100)
+    cvka.append(kappa * 100)
+    cvpre.append(prec * 100)
+    cvrec.append(rec * 100)
+    cvf1.append(f1 * 100)
+
+    ntrain += 1
+
+file_name = "classification_report_hyper3dnet" + dataset + ".txt"
+with open(file_name, 'w') as x_file:
+    x_file.write("Overall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvoa)), float(np.std(cvoa))))
+    x_file.write('\n')
+    x_file.write("Average accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvaa)), float(np.std(cvaa))))
+    x_file.write('\n')
+    x_file.write("Kappa accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvka)), float(np.std(cvka))))
+    x_file.write('\n')
+    x_file.write("Precision accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvpre)), float(np.std(cvpre))))
+    x_file.write('\n')
+    x_file.write("Recall accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvrec)), float(np.std(cvrec))))
+    x_file.write('\n')
+    x_file.write("F1 accuracy%.3f%% (+/- %.3f%%)" % (float(np.mean(cvf1)), float(np.std(cvf1))))
+
+# Calculate mean and std
+means = np.mean(confmatrices * 100, axis=0)
+stds = np.std(confmatrices * 100, axis=0)
+
+with open('meanshyper3dnet', 'wb') as f:
+    pickle.dump(means, f)
+with open('stdshyper3dnet', 'wb') as f:
+    pickle.dump(stds, f)
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Plot confusion matrix
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+with open('meanshyper3dnet', 'rb') as f:
+    means = pickle.load(f)
+with open('meanshyper3dnet', 'rb') as f:
+    stds = pickle.load(f)
+
+
+def plot_confusion_matrix(cm, cms, classescf,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.colorbar()
+    tick_marks = np.arange(len(classescf))
+    plt.xticks(tick_marks, classescf, rotation=45)
+    plt.yticks(tick_marks, classescf)
+
+    thresh = cm.max() / 2.
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+
+            if (cm[i, j] == 100 or cm[i, j] == 0) and cms[i, j] == 0:
+                plt.text(j, i, '{0:.0f}'.format(cm[i, j]) + '\n$\pm$' + '{0:.0f}'.format(cms[i, j]),
+                         horizontalalignment="center",
+                         verticalalignment="center", fontsize=15,
+                         color="white" if cm[i, j] > thresh else "black")
+
+            else:
+                plt.text(j, i, '{0:.2f}'.format(cm[i, j]) + '\n$\pm$' + '{0:.2f}'.format(cms[i, j]),
+                         horizontalalignment="center",
+                         verticalalignment="center", fontsize=15,
+                         color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# Plot non-normalized confusion matrix
+classes_list = list(range(0, int(3)))
+plt.figure()
+plot_confusion_matrix(means, stds, classescf=classes_list)
+dataset = 'WEED'
+plt.savefig('MatrixConfusion_' + dataset + 'hyper3dnet.png', dpi=1200)
