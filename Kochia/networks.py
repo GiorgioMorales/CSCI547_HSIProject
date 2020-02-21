a58abe1b09f92fba5d3dcf6950f1ae4c123a5ea2
GiorgioMorales <giorgiomoralesluna@gmail.com>
2020-02-21 01:31:35 -0700

    Updated metrics. KochiaCNN trained 20 bands.

--- Kochia/networks.py
+++ Kochia/networks.py
@@ -1,7 +1,8 @@
-from keras.layers import Conv3D, SeparableConv2D, Dense, Reshape, Flatten, ConvLSTM2D, Permute, Dropout, Conv2D, Add
-from keras.layers import Input, GlobalAveragePooling2D, AveragePooling3D, Lambda, Concatenate, Multiply, MaxPooling2D
+from keras.layers import Conv3D, SeparableConv2D, Dense, Reshape, Flatten, ConvLSTM2D, Permute, Dropout, Conv2D, DepthwiseConv2D
+from keras.layers import Input, GlobalAveragePooling2D, AveragePooling3D, Lambda, Concatenate, Multiply, MaxPooling2D, LSTM
 from keras.layers import BatchNormalization, Activation
 from keras.models import Model
+from keras import regularizers
 from keras.applications.resnet50 import ResNet50
 
 import keras.backend as k
@@ -130,5 +131,30 @@ def hyper3dnet_attention(img_shape=(256, 256, 50, 1), classes=2):
     return Model(d0, fc1)
 
 
-model = hyper3dnet_attention((25, 25, 300, 1), classes=3)
+def hyper3dnet_simplified(img_shape=(256, 256, 50, 1), classes=2):
+    # Input
+    d0 = Input(shape=img_shape)
+
+    conv_in = SeparableConv2D(128, kernel_size=5, strides=(1, 1), padding='same')(d0)
+    conv_in = BatchNormalization()(conv_in)
+    conv_in = Activation('relu')(conv_in)
+    conv_in = SeparableConv2D(128, kernel_size=3, strides=(2, 2), padding='same',
+                              dilation_rate=1)(conv_in)
+    conv_in = BatchNormalization()(conv_in)
+    conv_in = Activation('relu')(conv_in)
+    conv_in = SeparableConv2D(128, kernel_size=3, strides=(2, 2), padding='same',
+                              dilation_rate=1)(conv_in)
+    conv_in = Activation('relu')(conv_in)
+
+    conv_in = GlobalAveragePooling2D()(conv_in)
+
+    if classes == 2:
+        fc1 = Dense(1, name='fc' + str(1), activation='sigmoid')(conv_in)
+    else:
+        fc1 = Dense(classes, name='fc' + str(classes), activation='softmax')(conv_in)
+
+    return Model(d0, fc1)
+
+
+model = hyper3dnet_simplified((25, 25, 150), classes=3)
 model.summary()
